91dc36c3a6c093476b9e9c9d6cb243a206514a89
juaninicolai <juan0719@stud.kea.dk>
2022-06-21 16:13:11 +0200

    auth uncommented

--- server/app.js
+++ server/app.js
@@ -9,14 +9,27 @@ let corsOptions = {
 import cors from "cors";
 app.use(cors(corsOptions));
 
-// import authRoutes from "./routers/authRoutes.js";
-// app.use("/auth/login", authRoutes);
+import authRoutes from "./routers/authRoutes.js";
+app.use("/auth", authRoutes);
 
 import booksRoutes from "./routers/booksRoutes.js";
 app.use("/api", booksRoutes);
 
 import mailRouter from "./routers/mailRouter.js";
 app.use("/api/sendmail", mailRouter);
+
+//db connection
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+let uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
