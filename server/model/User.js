02c3c33b2b116ca17fba9e3b9abe0969c722276a
juaninicolai <juan0719@stud.kea.dk>
2022-06-21 11:07:14 +0200

    model added, not working yet

---
+++ server/model/User.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import isEmail from 'validator';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [ isEmail, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        require: [true, 'Please enter a password'],
+        minlength: [6, 'Minimun password length is 6 characters']
+    }
+});
+
+userSchema.post('save', function(doc, next) {
+    console.log('new user was created and saved', doc);
+    next();
+})
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password)
+      
+      if (auth) {
+        return user;
+      };
+      throw Error('Incorrect password');
+    };
+    throw Error('Incorrect email');
+}
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
