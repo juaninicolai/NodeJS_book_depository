5b61062cf7218b5b793abee51751513ff9472c91
juaninicolai <juan0719@stud.kea.dk>
2022-06-21 16:12:50 +0200

    routes updated

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import authController from '../controllers/authController.js';
+import { signup_get, signup_post, login_get, login_post, logout_get } from '../controllers/authController.js';
 
 const router = Router();
 
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
-router.get('/logout', authController.logout_get);
+router.get('/signup', signup_get);
+router.post('/signup', signup_post);
+router.get('/login', login_get);
+router.post('/login', login_post);
+router.get('/logout', logout_get);
 console.log("authRoutes.js loaded");
 
 export default router;
--- server/routers/booksRoutes.js
+++ server/routers/booksRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import booksController from '../controllers/booksController.js';
+import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from '../controllers/booksController.js';
 
 const router = Router();
 
-router.get('/books', booksController.get_all_books);
-router.get('/books/:id', booksController.get_book_by_id);
-router.post('/books', booksController.create_book);
-router.patch('/books/:id', booksController.update_book);
-router.delete('/books/:id', booksController.delete_book);
+router.get('/books',getAllBooks);
+router.get('/books/:id', getBookById);
+router.post('/books', createBook);
+router.patch('/books/:id', updateBook);
+router.delete('/books/:id', deleteBook);
 export default router;
