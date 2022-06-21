c65fa0f8e8f4279615c0cfcffd42bbaee15ddb38
juaninicolai <juan0719@stud.kea.dk>
2022-06-21 16:12:18 +0200

    models created

---
+++ server/models/Book.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    imageLink: String,
+    genre: String,
+    price: Number
+});
+
+const Book = mongoose.model("book", bookSchema);
+
+export default Book;
+
