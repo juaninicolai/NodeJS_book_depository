c32408d9c83f0f9aad43480cc0647788b88168bb
juaninicolai <juan0719@stud.kea.dk>
2022-06-24 09:27:13 +0200

    not implemented yet

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const requireAuth = (req, res, next) => {
+	const token = req.cookies.jwt;
+	if (token) {
+		jwt.verify(token, "secret token", (err, decodedToken) => {
+			if (err) {
+				console.log(err.message);
+				res.redirect("/login");
+			} else {
+				console.log(decodedToken);
+				next();
+			}
+		});
+	} else {
+		res.redirect("/login");
+	}
+};
+
+//check current user
+const checkUser = (req, res, next) => {
+	const token = req.cookies.jwt;
+	if (token) {
+		jwt.verify(token, "secret token", async (err, decodedToken) => {
+			if (err) {
+				console.log(err.message);
+				res.locals.user = null;
+			} else {
+				//console.log(decodedToken);
+				//req.decodedToken = decodedToken;
+				let user = await User.findById(decodedToken.id);
+				res.locals.user = user;
+				next();
+			}
+		});
+	} else {
+		res.locals.user = null;
+		next();
+	}
+};
+
+export { requireAuth };
