9bc7459d9d138328b25dc4270ddb96864c408cfe
juaninicolai <juan0719@stud.kea.dk>
2022-06-24 09:26:10 +0200

    db connection needs refactoring

--- server/app.js
+++ server/app.js
@@ -3,35 +3,38 @@ const app = express();
 app.use(express.json());
 
 let corsOptions = {
-  origin: ["http://localhost:8080"],
+	origin: ["http://localhost:8080"],
+	credentials: true,
 };
-
 import cors from "cors";
 app.use(cors(corsOptions));
 
-import authRoutes from "./routers/authRoutes.js";
+import cookieParser from "cookie-parser";
+app.use(cookieParser());
+
+import authRoutes from "./routes/authRoutes.js";
 app.use("/auth", authRoutes);
 
-import booksRoutes from "./routers/booksRoutes.js";
+import booksRoutes from "./routes/booksRoutes.js";
 app.use("/api", booksRoutes);
 
-import mailRouter from "./routers/mailRouter.js";
+import mailRouter from "./routes/mailRouter.js";
 app.use("/api/sendmail", mailRouter);
 
-//db connection
-import dotenv from 'dotenv';
-dotenv.config();
-import mongoose from 'mongoose';
+//would like to have this in a separate file
+import "dotenv/config";
+import mongoose from "mongoose";
 let uri = process.env.MONGO_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.log(err));
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+	console.log(`Server is listening on port ${PORT}`);
 });
