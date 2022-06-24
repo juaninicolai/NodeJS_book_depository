1ecc43085706f339735ae28921d7e3773ab79413
juaninicolai <juan0719@stud.kea.dk>
2022-06-24 09:28:41 +0200

    unused function commented out

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -3,46 +3,46 @@ import Book from '../models/book.js';
 export const getAllBooks = async (req, res) => {
     try {
         const books = await Book.find();
-        res.status(200).json(books);
+        res.status(200).json({ data: books });
     } catch (error) {
           res.status(500).json({error: 'Could not fetch the documents'});
     };
 };
 
-export const getBookById = async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id);
-        res.status(200).json(book);
-    } catch (error) {
-        res.status(500).json({error: 'Could not fetch the documents'});
-    }
-};
+// export const getBookById = async (req, res) => {
+//     try {
+//         const books = await Book.findById(req.params.id);
+//         res.status(200).json(books);
+//     } catch (error) {
+//         res.status(500).json({error: 'Could not fetch the documents'});
+//     }
+// };
 
-export const createBook = async (req, res) => {
-    try {
-        const book = await Book.create(req.body);
-        res.status(201).json(book);
-    } catch (error) {
-        res.status(500).json({error: 'Could not create the document'});
-    }
-};
+// export const createBook = async (req, res) => {
+//     try {
+//         const book = await Book.create(req.body);
+//         res.status(201).json(book);
+//     } catch (error) {
+//         res.status(500).json({error: 'Could not create the document'});
+//     }
+// };
 
-export const updateBook = async (req, res) => {
-    try {
-        const book = await Book.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        res.status(200).json(book);
-    } catch (error) {
-        res.status(500).json({error: 'Could not update the document'});
-    }
-};
+// export const updateBook = async (req, res) => {
+//     try {
+//         const book = await Book.findByIdAndUpdate(req.params.id, req.body, {new: true});
+//         res.status(200).json(book);
+//     } catch (error) {
+//         res.status(500).json({error: 'Could not update the document'});
+//     }
+// };
 
-export const deleteBook = async (req, res) => {
-    try {
-        await Book.findByIdAndDelete(req.params.id);
-        res.status(204).json({message: 'Document deleted'});
-    } catch (error) {
-        res.status(500).json({error: 'Could not delete the document'});
-    }
-};
+// export const deleteBook = async (req, res) => {
+//     try {
+//         await Book.findByIdAndDelete(req.params.id);
+//         res.status(204).json({message: 'Document deleted'});
+//     } catch (error) {
+//         res.status(500).json({error: 'Could not delete the document'});
+//     }
+//};
 
 export default Book;
